ab7c04d866d8c096fa7d8dc16a02796eb53db591
pdu05121-star <pdu05121@gmail.com>
2026-07-26 20:17:46 +0900

    Update ChuncheonTour.tsx

--- src/app/pages/ChuncheonTour.tsx
+++ src/app/pages/ChuncheonTour.tsx
@@ -1,3 +1,6 @@
+useEffect(() => {
+  (window as any).gtag?.('event', 'tour_detail_view', { region: 'chuncheon' });
+}, []);
 import { useState } from "react";
 import { Link } from "react-router";
 import { ChevronLeft, MapPin, Sparkles } from "lucide-react";
